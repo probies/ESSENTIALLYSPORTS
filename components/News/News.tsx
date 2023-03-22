688c71506fdb385b8373ec2d8192589e66d9a2b9
Piyush Jha <jhapiyush06@gmail.com>
2023-03-22 21:16:47 +0530

    Added Key

--- components/News/News.tsx
+++ components/News/News.tsx
@@ -8,7 +8,7 @@ function News({ newsList }: any) {
       {
         newsList?.map((news: any, index: number) => {
           return (
-            <NewsCard news={news} key={index} />
+            <NewsCard news={news} index={index} key={index}/>
           )
         })
       }
